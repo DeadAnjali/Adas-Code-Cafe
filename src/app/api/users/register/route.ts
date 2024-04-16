2a13ae395ce0ff69a3e09080b997e410165bf342
DeadAnjali <anjalimahere15@gmail.com>
2024-04-16 22:12:04 +0530

    signup-route

---
+++ src/app/api/users/register/route.ts
@@ -0,0 +1,46 @@
+import { connect } from "@/dbconfig/dbconfig";
+import User from '@/model/userModel'
+import { NextRequest,NextResponse } from "next/server";
+import bcrypt from 'bcrypt';
+
+
+
+
+
+connect();
+export async function post(request:NextRequest) {
+    try {
+        const reqbody=await request.json()
+        const{name,regno,email,password}=reqbody;
+        console.log(reqbody);
+        //check if user already exist
+
+        const user=await User.findOne({regno});
+        if(user){
+            return NextResponse.json({error:"User already exists"},{status:400})
+        }
+
+        //hash password
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
+
+        const newUser=new User({
+            name,
+            regno,
+            email,
+            password:hashedPassword
+        })
+        const saveduser=await newUser.save();
+        console.log(saveduser);
+        return NextResponse.json({
+            message:"User created Successfully",
+            success:true,
+            saveduser
+        })
+
+    } catch (error:any) {
+        return NextResponse.json({
+            error:error.message
+        },{status:500})
+    }
+}
