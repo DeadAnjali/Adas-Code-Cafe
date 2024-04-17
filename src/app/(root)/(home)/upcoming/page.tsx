6e4291e1677e1000590dcccdcc9ad88bdef02985
DeadAnjali <anjalimahere15@gmail.com>
2024-04-17 23:54:11 +0530

    first-commit-to-clerk-branch

---
+++ src/app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Upcoming = () => {
+  return (
+    <section className="flex size-full flex-col gap-3 text-white">
+      <div>
+        Upcoming
+      </div>
+    </section>
+  )
+}
+
+export default Upcoming
